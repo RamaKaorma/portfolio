8e019e79703fa7a41aeb87fbb81cba38db336b15
RamaKaorma <rkaorma@student.unimelb.edu.au>
2026-02-25 15:07:24 +1100

    removed schema import from Skills.jsx

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -6,7 +6,6 @@ import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import { Tooltip } from 'react-tooltip';
 import './Skills.scss';
-import { workExperience } from '../../../../backend/schemaTypes/workExperience';
 
 
 const Skills = () => {
